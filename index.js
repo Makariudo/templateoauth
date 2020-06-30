42eb4e80fa7de4e707816d3d618ce2d4f2c15294
Makariudo <sspassevitch@gmail.com>
2020-06-30 14:04:40 +0200

    changed greeting

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ bye: 'buddy' })
 })
 
-const PORT = process.env.PORT || 5000; //ecoute sur notre site de deploy(heroku ici) ou par defaut port 5000
-app.listen(PORT);
+const PORT = process.env.PORT || 5000 //ecoute sur notre site de deploy(heroku ici) ou par defaut port 5000
+app.listen(PORT)
